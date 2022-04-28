b0733fa49df4c90de7939916d60deb0629305d6e
AmithRV <amithrajv1999@gmail.com>
2022-04-28 21:37:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,184 @@
+class SearchEngine {
+    constructor() {
+        this.imageArray = 0;
+        this.paginationUrl; // sets the base url 
+
+        this.loadURL = async function (url) {
+            let result = [];
+            await fetch(url)
+                .then(response => response.json())
+                .then(commits => {
+                    commits.map((e) => { result.push(e) })
+                });
+            return result;
+        };
+
+        this.pageCount = function (url, limit) {
+            console.log('url : ', url);
+            //
+            fetch(url, {    method: 'get',
+                            headers: {
+                                        "x-api-key": '171d3563-d945-4e75-90f4-c545359d94fe',
+                            },
+                }).then(response => {
+                        let totalPage = parseInt(response.headers.get("pagination-count") );
+                        console.log('length : ', totalPage);
+                        if (totalPage === 0) {       // create a page when no result is found
+                            console.log('result is empty');
+                            const emptyPage = document.createElement('div');
+                            emptyPage.className = 'emptyPage';
+                            emptyPage.innerHTML = 'NO RESULT FOUND';
+                            const imageCollection = document.getElementById('imageCollection');
+                            imageCollection.append(emptyPage);
+                            this.setPage(totalPage, limit);
+                        } else {
+                            this.setPage(totalPage, limit)
+                        }
+                })
+        };
+
+        this.setPage = function (imageCount, limit) {
+            let pages = parseInt(imageCount / limit);
+            const list = document.getElementById("pagination");
+
+            while (list.hasChildNodes()) {
+                list.removeChild(list.firstChild);
+            }
+
+            for (let i = 1; i <= pages; i++) {
+                const page = document.createElement('a');
+                page.className = 'page';
+                page.innerHTML = i;
+                page.tabIndex = '1';
+                page.onclick = () => { this.loadPage(i, limit) }
+                const pageCover = document.getElementById('pagination');
+                pageCover.append(page);
+            }
+        };
+
+        this.loadPage = function (pageNumber, limit) {
+            let baseUrl = this.paginationUrl;
+            let newUrl = baseUrl + `&limit=${limit}&page=${pageNumber}`;
+            console.log(`baseurl :  ${newUrl}`)
+            const list = document.getElementById("imageCollection");
+            while (list.hasChildNodes()) {
+                list.removeChild(list.firstChild);
+            }
+
+            let result = this.loadURL(newUrl);
+            result.then((e) => {
+                e.map((element) => {
+                    const image = document.createElement("img");
+                    image.src = element.url;
+                    let cover = document.createElement("div");
+                    cover.className = 'image_container'
+                    cover.append(image);
+                    document.getElementById('imageCollection').append(cover)
+                });
+            })
+        }
+    }
+}
+
+class BreedSelector {
+    constructor() {
+        fetch('https://api.thecatapi.com/v1/breeds')
+            .then(response => response.json())
+            .then(commits => {
+                commits.map(
+                    (e) => {
+                        let option = document.createElement('option');
+                        option.innerHTML = `${e.name}`;
+                        option.value = `${e.id}`
+                        document.getElementById('breedSelector').append(option);
+                    })
+            });
+    }
+};
+
+class CategorySelector {
+    constructor() {
+        fetch('https://api.thecatapi.com/v1/categories')
+            .then(response => response.json())
+            .then(commits => {
+                commits.map(
+                    (e) => {
+                        let option = document.createElement('option');
+                        option.innerHTML = `${e.name}`;
+                        option.value = `${e.id}`
+                        document.getElementById('categorySelector').append(option);
+                    })
+            });
+    }
+}
+
+let defaultLoader = function () {
+    new BreedSelector();
+    new CategorySelector();
+    let search = new SearchEngine();
+
+    let loader = function (event) {
+        let categorySelector = document.getElementById('categorySelector');
+        let breedSelector = document.getElementById('breedSelector');
+        let typeSelector = document.getElementById('typeSelector');
+        let orderSelector = document.getElementById('orderSelector');
+        let limitSelector = document.getElementById('limitSelector');
+
+        let category = categorySelector.options[categorySelector.selectedIndex].value;
+        let breed = breedSelector.options[breedSelector.selectedIndex].value;
+        let type = typeSelector.options[typeSelector.selectedIndex].value;
+        let order = orderSelector.options[orderSelector.selectedIndex].value;
+        let limit = limitSelector.options[limitSelector.selectedIndex].value;
+
+        if (order === 'null') {
+            order = 'random';
+        } if (type === 'null') {
+            type = 'All';
+        } if (limit === 'null') {
+            limit = 9;
+        } if (category == "none") {
+            category = '';
+        } if (breed == "none") {
+            breed = '';
+        }
+
+        const list = document.getElementById("imageCollection");
+        while (list.hasChildNodes()) {
+            list.removeChild(list.firstChild);
+        }
+
+        let url = `https://api.thecatapi.com/v1/images/search?limit=${limit}&order=${order}&mime_types=${type}&category_ids=${category}&breed_ids=${breed}`
+        let result = search.loadURL(url);
+        result.then((e) => {
+            e.map((element) => {
+                const image = document.createElement("img");
+                image.src = element.url;
+                let cover = document.createElement("div");
+                cover.className = 'image_container'
+                cover.append(image);
+                document.getElementById('imageCollection').append(cover)
+            });
+        })
+        search.paginationUrl = `https://api.thecatapi.com/v1/images/search?order=${order}&mime_types=${type}&category_ids=${category}&breed_ids=${breed}`; // sets the base url which does not contain limit
+        search.pageCount(`https://api.thecatapi.com/v1/images/search?limit=${limit}&order=${order}&mime_types=${type}&category_ids=${category}&breed_ids=${breed}`, limit);  // setting the pagecount 
+    };
+
+    (function () {
+        let temp = document.getElementById('navBar').getElementsByTagName('button')[0];
+        temp.addEventListener('click', loader);
+
+        let searchResult = search.loadURL(`https://api.thecatapi.com/v1/images/search?limit=9`);
+        searchResult.then((e) => {
+            e.map((element) => {
+                const image = document.createElement("img");
+                image.src = element.url;
+                let cover = document.createElement("div");
+                cover.className = 'image_container';
+                cover.append(image);
+                document.getElementById('imageCollection').append(cover)
+            })
+        })
+    })();
+}
+
+window.onload = defaultLoader;
